8a40bb69292159dbbea3b8395990b09f4093dbcb
ShyamaHarihar <shyama.harihar@gmail.com>
2022-03-24 19:57:01 +0530

    styling and bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
-import 'package:flutter/material.dart';
 import 'package:seproject/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBkAEk0oaxy3NpYpw0Bq8vukbjPf2mJMpI",
+      appId: "1:254160548454:android:c9fb3bdd4d15dca63333a7",
+      messagingSenderId: "254160548454",
+      projectId: "seproject-667e1",
+    ),
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Profile Management',
+      title: 'Email And Password Login',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
 }
-
 
